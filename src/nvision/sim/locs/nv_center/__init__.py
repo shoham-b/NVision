902721a50ce437f048d8dcdd56536a3c617b1bc5
Shoham Baris <shoham.baris@gmail.com>
2025-11-22 22:59:19 +0200

    add simple bayesian

--- src/nvision/sim/locs/nv_center/__init__.py
+++ src/nvision/sim/locs/nv_center/__init__.py
@@ -1,11 +1,15 @@
 from nvision.sim.locs.nv_center._bayesian_adapter import NVCenterSequentialBayesianLocatorBatched
+from nvision.sim.locs.nv_center._simple_adapter import SimpleSequentialLocatorBatched
 from nvision.sim.locs.nv_center.sweep_locator import NVCenterSweepLocator
 
-# Alias the batched version as the main class name for backward compatibility
+# Alias the batched versions as the main class names for backward compatibility
 NVCenterSequentialBayesianLocator = NVCenterSequentialBayesianLocatorBatched
+SimpleSequentialLocator = SimpleSequentialLocatorBatched
 
 __all__ = [
     "NVCenterSequentialBayesianLocator",
     "NVCenterSequentialBayesianLocatorBatched",
     "NVCenterSweepLocator",
+    "SimpleSequentialLocator",
+    "SimpleSequentialLocatorBatched",
 ]
