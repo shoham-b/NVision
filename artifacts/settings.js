7611e2fa842d4c1911a96a39e355a07826008421
Shoham Baris <shoham.baris@gmail.com>
2026-03-17 17:55:38 +0200

    Update UI

--- artifacts/settings.js
+++ artifacts/settings.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3c73a6a1e3fb2cde4148ad3fdf180c264103329e341addcafa65b74ce8c6f215
+oid sha256:ad3c2c88a07170ed4033c98ec91dbd82b0e3e011cc00f7245307562f80f1be35
 size 133
